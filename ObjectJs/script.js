a4aaa653cc07eb1d53a4640cac00024fbf5969af
Vanshverma543 <anitajii456@gmail.com>
2026-05-06 20:19:29 +0530

    js

---
+++ ObjectJs/script.js
@@ -0,0 +1,10 @@
+let verma ={
+    "name": "verma Verma",
+    "dob": "2002-05-14",
+    "mobile": "9876543210",
+    "city": "Bhopal",
+    "job": "Software Developer"
+  }
+
+console.log(Object.keys(vansh));
+console.log(Object.values(verma));
