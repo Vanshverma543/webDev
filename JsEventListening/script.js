0b47d32c9a865d15243d95c852170e4a7fc4a83d
Vanshverma543 <anitajii456@gmail.com>
2026-05-08 20:32:35 +0530

    js

---
+++ JsEventListening/script.js
@@ -0,0 +1,36 @@
+document.getElementById("redbox").addEventListener("mouseover",fillRedColor);
+document.getElementById("redbox").addEventListener("mouseout",fillBaseColor);
+
+document
+.getElementById("greenbox");
+.addEventListener("mouseover", fillGreenColor);
+
+document.getElementById("greenbox").addEventListener("mouseout",fillBaseColor);
+
+document.getElementById("bluebox").addEventListener("mouseover",fillBlueColor);
+document.getElementById("bluebox").addEventListener("mouseout",fillBaseColor);
+
+function fillredcolor(){
+    
+document.getElementById("bulb1").style.backgroundColor = "red";
+}
+
+function fillredcolor(){
+    
+document.getElementById("bulb1").style.backgroundColor = "red";
+}
+function fillredcolor(){
+    
+document.getElementById("bulb1").style.backgroundColor = "green";
+}
+
+function fillredcolor(){
+    
+document.getElementById("bulb1").style.backgroundColor = "blue";
+}
+
+function fillredcolor(){
+    
+document.getElementById("bulb1").style.backgroundColor = "white";
+}
+
