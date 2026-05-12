c1b385af72c1f7e3d001942265a553369d4d615f
Vanshverma543 <anitajii456@gmail.com>
2026-05-12 20:12:40 +0530

    js

---
+++ AgeCalculator/script.js
@@ -0,0 +1,7 @@
+let fn = document.getElementById("fullname").value;
+
+let pn = document.getElementById("phonenumber").value;
+
+let en = document.getElementById("fullname").value;
+
+let pw = document.getElementById("phonenumber").value;
