c181a9b4cd3c53c772fa156f388368151ee06297
Vanshverma543 <anitajii456@gmail.com>
2026-06-01 20:39:15 +0530

    react

--- frontend/MyFirstReactApp/src/App.jsx
+++ frontend/MyFirstReactApp/src/App.jsx
@@ -1,15 +1,11 @@
 import Header from "./components/header";
-import Footer from "./components/footer";
+// import Footer from "./components/footer";
 function App() {
   return(
     <>
     <Header/>
-      <div id ="abcd">My First React App</div>
-
-      <p className ="bg-danger">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi repellat
-         enim fuga fugiat quas aliquam alias obcaecati incidunt numquam similique.</p>
-     <Footer/>
+    
+     
     </>
   );
 }
--- frontend/MyFirstReactApp/src/components/Header.jsx
+++ frontend/MyFirstReactApp/src/components/Header.jsx
@@ -1,8 +1,20 @@
+import { link } from "react-router-dom"
 function Header() {
-    return (
-        <>
-           <div id="header">This Is header</div> 
-        </>   
-    );
+  return (
+    <>
+      <div className="bg-primary-subtle p-2 d-flex justify-content-between align-items-center">
+        <div className="text-primary fs-4 fw-bold">My Company</div>
+
+        <div className="d-flex gap-4">
+         
+        </div>
+
+        <div className="d-flex gap-3">
+          <button className="btn btn-outline-primary">Login</button>
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Header;
