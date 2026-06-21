1812314a0dfd9ecb487459673eab33c36f41d732
Vanshverma543 <anitajii456@gmail.com>
2026-06-21 17:12:15 +0530

    backend

---
+++ Backend/FirstBackend/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Default Get API Hit");
+  res.json({ message: "Welcome to my first backend Project" });
+});
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server Started on port:", port);
+});
