d6c7aa3007157192a59f3038c55fb31326ba67d9
Vanshverma543 <anitajii456@gmail.com>
2026-06-22 20:23:10 +0530

    backend

--- Backend/FirstBackend/index.js
+++ Backend/FirstBackend/index.js
@@ -1,15 +1,35 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import AuthRouter from "/src/routers/auth.route.js"
+import PublicRouter from "/src/routers/public.route.js"
 
 const app = express();
+
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
 
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
   res.json({ message: "Welcome to my first backend Project" });
 });
 
+app.port("/register", (req, res) => {
+  res.json({ message: "login successfull"});
+});
 
+app.port("/logout", (req, res) => {
+  res.json({ message: "logout successful"});
+});
+
+app.port("/register", (req, res) => {
+  res.json({ message: "logout sucessfull"});
+});
+
+app.port("/register", (req, res) => {
+  res.json({ message: "login sucessfull"});
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
---
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const loginUser = (req, res) =>{
+    res.json({ message: "Login Successfull from controller "});
+};
+
+export const RegisterUser = (req, res) =>{
+    res.json({ message: "Register Successfull from controller "});
+};
+
+export const logoutUser = (req, res) =>{
+    res.json({ message: "Logout Successfull from controller "});
+};
