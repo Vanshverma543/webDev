e53f1f569dc1296b097440b3dd807e226e24f861
Vanshverma543 <anitajii456@gmail.com>
2026-06-26 20:28:36 +0530

    backend login

--- Backend/FirstBackend/src/controllers/auth.controller.js
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -5,7 +5,8 @@ export const RegisterUser = async (req, res) => {
     const { fullName, email, password, phone, gender, dob } = req.body;
 
     if (!fullName || !email || !password || !phone || !gender || !dob) {
-      res.status(400).json({ message: "All Feilds Required" });
+      const error = new Error("All field Required");
+
       return;
     }
 
@@ -15,8 +16,27 @@ export const RegisterUser = async (req, res) => {
       return;
     }
 
-    //Create New User and Compleyte registration will do this tomorrow
+    const photoUrl =
+      "https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()";
+
+    const photo = {
+      url: photoUrl,
+      publicId: null,
+    };
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
+      phone,
+      gender,
+      dob,
+      photo,
+    });
+
+    res.status(201).json({ message: "User Created Successfully" });
   } catch (error) {}
+  res.status(500).json({ message: "Internal Server Errror" });
 };
 
 export const LoginUser = (req, res) => {
@@ -26,3 +46,25 @@ export const LoginUser = (req, res) => {
 export const LogoutUser = (req, res) => {
   res.json({ message: "Logout Successfull from Controller" });
 };
+if (!email || !password) {
+  const error = new Error("All filed Required")
+  error.statusCode = 400;
+  return next (error);
+}
+
+if (!email || !password) {
+  const error = new Error("All filed Required")
+  error.statusCode = 404;
+  return next (error);
+}
+
+if(password !== existingUser.password) {
+  const error = new Error("Incorrect Password");
+  error.status =401;
+  return next(error);
+}
+
+res.status(200).json({
+  message:"Welcome Back",
+  data: existingUser,
+});
